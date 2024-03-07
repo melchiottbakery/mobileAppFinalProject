f574b1a75cec47e4821b16eaa97a9897d61a8b7c
melchiottbakery <ysregister@outlook.com>
2024-03-07 15:10:39 -0800

    change of the name of the folder

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet } from "react-native";
-import Login from "./screens/Login";
+import Login from "./Screens/Login";
 
 export default function App() {
   return <Login />;
