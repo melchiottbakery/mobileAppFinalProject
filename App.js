a27699d322e563b8fa6d33450d8d62669d6a3906
tz liao <tzliao@tzdeMacBook-Pro.local>
2024-03-08 13:26:34 -0800

    install navigator package, change folder name screens

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet } from "react-native";
-import Login from "./Screens/Login";
+import Login from "./screens/Login";
 
 export default function App() {
   return <Login />;
