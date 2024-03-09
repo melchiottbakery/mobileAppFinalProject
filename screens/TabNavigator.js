14487133afdafd7988b42058953212c99ff1cab8
tz liao <tzliao@tzdeMacBook-Pro.local>
2024-03-08 19:47:18 -0800

    build BottomTab navigator, library button, 4 tab screens

--- screens/TabNavigator.js
+++ screens/TabNavigator.js
@@ -1,12 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Button } from "react-native";
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Profile from "./Profile";
+import Library from "./Library";
+import MyList from "./MyList";
+import Welcome from "./Welcome";
+
+const Tab = createBottomTabNavigator();
 
 export default function TabNavigator() {
   return (
-    <View>
-      <Text>TabNavigator</Text>
-    </View>
-  )
+    <Tab.Navigator>
+      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen name="Welcome" component={Welcome} />
+      <Tab.Screen
+        name="Library"
+        component={Library}
+        options={{
+          headerRight: () => (
+            <Button
+              title="Add"
+              onPress={() => console.log("Add button pressed")}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen name="MyList" component={MyList} />
+    </Tab.Navigator>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
