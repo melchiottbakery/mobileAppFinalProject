ee4afee510c662aabce3947fae1d3592d817718b
melchiottbakery <ysregister@outlook.com>
2024-03-11 05:56:35 -0700

    add some features for the screens

--- screens/Registration.js
+++ screens/Registration.js
@@ -5,6 +5,23 @@ export default function Registration() {
   return (
     <View>
       <Text>This is the Registration screen</Text>
+
+      <Text>a InputComponent input the nickname</Text>
+      <Text>a InputComponent input the email</Text>
+      <Text>a InputComponent confirm the email</Text>
+      <Text>a InputComponent input the passpord</Text>
+      <Text>a InputComponent confirm the password</Text>
+
+      <Text>a button cancel and go back</Text>
+      <Text>a button Registration</Text>
+
+
+
+
+
+
+
+
     </View>
   )
 }
