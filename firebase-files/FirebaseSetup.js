4e4720184eaf724cb5c355738398d43fa9634008
melchiottbakery <ysregister@outlook.com>
2024-03-28 17:36:13 -0700

    change the config of firebase as .env

--- firebase-files/FirebaseSetup.js
+++ firebase-files/FirebaseSetup.js
@@ -2,18 +2,33 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+import {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+} from "@env";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCM9oLreQ76KUcldfw9GvEjNglgEjeb5fM",
-  authDomain: "groupproject-e9078.firebaseapp.com",
-  projectId: "groupproject-e9078",
-  storageBucket: "groupproject-e9078.appspot.com",
-  messagingSenderId: "539915644722",
-  appId: "1:539915644722:web:d6f28e6f874303c99c312b"
+  apiKey: apiKey,
+  authDomai: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
 };
+
+// 
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
