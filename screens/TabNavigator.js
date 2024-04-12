bb4a38e770bca43cde2e4e7603be7752aa215a99
melchiottbakery <ysregister@outlook.com>
2024-04-11 19:00:50 -0700

    add for readme.md

--- screens/TabNavigator.js
+++ screens/TabNavigator.js
@@ -11,59 +11,12 @@ import { getProfile } from "../firebase-files/FirebaseHelper";
 import { auth } from '../firebase-files/FirebaseSetup';
 
 
-// const [jsonUrl, setJsonUrl]= useState('');
-
-
-
 
 export default function TabNavigator() {
 
 
-  // function showAlertWithTextInput(){
 
-  // };
-  
-  async function fetchUsers(inputText) {
-    try { 
-      const response = await fetch(
-        inputText
-    );
-    if (!response.ok) {
-      throw new Error("data wasn't there!"); 
-    }
-    const data = await response.json()
-    console.log(data)
-    } 
-    catch (error) {
-      console.log("fetch users ", error);
-    }   
-  }
-  
-
-
-  function showAlertWithTextInput(){
-    // Alert.alert('Email or Password is not correct ')
-    // andriod cannot 
-    Alert.prompt();
-
-    Alert.prompt(
-      'Add a dictionary to the library',
-      'Please enter url of json file:',
-      (inputText) => {
-        if (inputText) {
-          // Handle the text input here
-          fetchUsers(inputText)
-          console.log('You entered: ' + inputText);
-        }
-       
-      },
-
-      );
-   
-  };
   // "https://jsonplaceholder.typicode.com/users"
-
-
 
 
   return (
@@ -74,14 +27,7 @@ export default function TabNavigator() {
         name="Library"
         component={Library}
         options={{
-          headerRight: () => (
-            <Button
-              title="Add"
-              // onPress={() => console.log("Add button pressed")}
-              onPress={showAlertWithTextInput}
-
-            />
-          ),
+          
         }}
       />
       <Tab.Screen name="MyList" component={MyList} />
