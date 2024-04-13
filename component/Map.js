f4baeec948a9e4c82e5f7e5060e5a45582ab2c8f
tz liao <tzliao@tzdeMacBook-Pro.local>
2024-04-13 10:36:19 -0700

    show user's current location in map

--- component/Map.js
+++ component/Map.js
@@ -1,21 +1,56 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
+import { mapsApiKey } from "@env";
 
 export default function Map() {
+    const [location, setLocation] = useState(null);
+  //const [schools, setSchools] = useState([]);
+  const [mapRegion, setMapRegion] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+//   const [status, requestPermission] = Location.useForegroundPermissions();
+//   async function verifyLocationPermission() {
+//     if (status.granted) {
+//       return true;
+//     }
+//     try {
+//       const permissonResponse = await requestPermission();
+//       return permissonResponse.granted;
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }
+useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        Alert.alert('Permission Denied', 'Sorry, we need location permissions');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location.coords);
+      setMapRegion({
+        ...mapRegion,
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      });
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
       <MapView
         style={styles.map}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
+        region={mapRegion}
         provider="google"
       >
-        <Marker coordinate={{ latitude: 37.78825, longitude: -122.4324 }} />
+        {location &&  (<Marker coordinate={{ latitude: location.latitude, longitude: location.longitude }} /> )}
       </MapView>
     </View>
   );
